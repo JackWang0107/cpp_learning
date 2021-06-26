2f08463472cef73e2545eb955004ac4b622ed55c
Jack <2232123545@qq.com>
2021-06-26 14:34:55 +0800

    finished chapter4 -- By Jack Wang 2021.6.26 14:34

---
+++ chapter4/assgn_st.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct inflatable
+{
+	char name[20];
+	float volume;
+	double price;
+};
+
+int main(){
+	using namespace std;
+
+	inflatable bouquet = {
+		"sunflower",
+		0.20,
+		12.4
+	};
+
+	inflatable choice;
+
+	cout << "bouquet: " << bouquet.name << " for $" << bouquet.price << endl;
+	
+	choice = bouquet;
+	cout << "choice: " << choice.name << " for $" << choice.price << endl;
+
+	return 0;
+}
---
+++ chapter4/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+	using namespace std;
+	cout << R"(Jim "King" Tutt uses "\n" instead of endl.)" << endl;
+	cout << R"+( To use (min) in raw string )+" << endl;
+
+	return 0;
+}
