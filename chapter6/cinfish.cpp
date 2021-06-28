e7fd9c5dd543c0f545d546f4e0b029522eff17ce
Jack Wang <2232123545@qq.com/jack3shihong@gmail.com>
2021-06-28 22:28:01 +0800

    finished chapter 6 -- By Jack Wang 2021.6.28 22:27

--- chapter6/cinfish.cpp
+++ chapter6/cinfish.cpp
@@ -8,19 +8,16 @@ int main(){
 	double fish[Max];
 	cout << "Please enter the weights of your fish.\n";
 	cout << "You may enter up tp " << Max << " fish <q to terminate>.\n";
+
 	cout << "fish #1: ";
-	
 	int i = 0;
-	while (++i < Max)
-	{
-		if (++ i < Max)
+	while (i < Max and cin >> fish[i])
+		if ( ++i < Max)
 			cout << "fish #" << i+1 << ": ";
-	}
 
 	double total = 0.0;
-	for (int j = 0; j < i; j++){
+	for (int j = 0; j < i; j++)
 		total += fish[j];
-	}
 
 	if (i == 0)
 		cout << "No fish\n";
