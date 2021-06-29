e43d04cb5f63e8dce66efdffc711020eb573e532
Jack Wang <2232123545@qq.com/jack3shihong@gmail.com>
2021-06-29 11:25:01 +0800

    finished part of chapter7 -- By Jack Wang 2021.6.29 11:24

---
+++ chapter7/calling.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void simple();
+
+
+int main(void){
+	using namespace std;
+
+	cout << "main() will call the simple() function:\n";
+	simple();
+
+	return 0;
+}
+
+void simple(){
+	using namespace std;
+	cout << "I'm but a simple function.\n";
+}
